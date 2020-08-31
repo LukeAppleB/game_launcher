e5aadc73009fc1b9ae8294914d97ceffd41d0147
Luke A <lukeappleb@gmail.com>
2020-08-30 22:28:20 -0300

    added list sorting, game launching

--- game_launcher.py
+++ game_launcher.py
@@ -1,18 +1,25 @@
 # need to read game json, display error if any
 import json
+import subprocess
 
 
 def library(games):
+    games['Games'] = sorted(games['Games'], key=lambda x: x['Title'])
     for game in games['Games']:
         print(game['Title'])
 
-def cmd_is_game_title(input_title: str):
-    pass
+
+def get_game_location(input_title: str, games):
+    for game in games['Games']:
+        if str.lower(input_title) == str.lower(game['Title']):
+            return game['Location']
+    print(f'No game found with name: {input_title}')
+    return None
+
 
 print('Welcome to Game Launcher')
 print('Please enter the name of the game you would like to launch, or "list", "help", or "exit" to close')
 cmd = None
-
 
 while cmd != 'exit':
     cmd = input()
@@ -22,15 +29,7 @@ while cmd != 'exit':
 
         if cmd == 'list':
             library(games)
-
-        if cmd_is_game_title(cmd):
-            pass
-
-
-
-
-# prompt for either list or name of game, or add/remove
-
-# have tab autofill similar to unix
-
-
+        else:
+            game_location = get_game_location(cmd, games)
+            if game_location is not None:
+                subprocess.run(game_location)
